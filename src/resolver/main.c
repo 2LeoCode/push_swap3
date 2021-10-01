c9949c81da6d0e1370b82fac76b943467459aa31
2LeoCode <leo.13suardi@outlook.com>
2021-10-01 10:18:16 +0200

    tolino

--- src/resolver/main.c
+++ src/resolver/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lsuardi <lsuardi@student.42.fr>            +#+  +:+       +#+        */
+/*   By: Leo Suardi <lsuardi@student.42.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/06 22:43:55 by leo               #+#    #+#             */
-/*   Updated: 2021/09/30 17:17:11 by lsuardi          ###   ########.fr       */
+/*   Updated: 2021/10/01 00:51:23 by Leo Suardi       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,18 @@
 
 #define ST_A 0
 #define ST_B 1
+
+void kek(void)
+{
+	write(1, "kek\n", 4);
+}
+
+void	print_stack(t_stack *st)
+{
+	printf("=====\n");
+	for (int *ptr = stkbegin(st); ptr != stkend(st); ++ptr)
+		printf("%d\n", *ptr);
+}
 
 int	alloc_queue(t_queue **queue, size_t size)
 {
@@ -43,17 +55,30 @@ void	add_op(const char *(*op)(t_stack *, t_stack *), t_stack *a,
 	t_stack *b, t_queue *instr_queue)
 {
 	op(a, b);
+	printf("=====\n");
+	printf("Op: %s\n", op(NULL, NULL));
 	enqueue(instr_queue, op(NULL, NULL));
+	print_stack(a);
+	print_stack(b);
+	usleep(500000);
 }
 
 void	reorderstart(t_stack *cur, t_stack *st[2], const char *(*moveset[4])(),
 			t_queue *instr_queue)
 {
-	int	*rbegin;
+	const int	*rbegin;
 
-	rbegin = stkrbegin(cur);
-	while (*rbegin < stktop(cur))
-		add_op(moveset[ROT], st[ST_A], st[ST_B], instr_queue);
+	rbegin = stkrcbegin(cur);
+	if (cur == st[ST_A])
+	{
+		while (*rbegin < stktop(cur))
+			add_op(moveset[RROT], st[ST_A], st[ST_B], instr_queue);
+	}
+	else
+	{
+		while (*rbegin > stktop(cur))
+			add_op(moveset[RROT], st[ST_A], st[ST_B], instr_queue);
+	}
 }
 
 ptrdiff_t	ft_ptrdiff(void *a, void *b)
@@ -61,6 +86,27 @@ ptrdiff_t	ft_ptrdiff(void *a, void *b)
 	if (a < b)
 		return (b - a);
 	return (a - b);
+}
+
+int	*next_streak(t_stack *cur, t_stack *st[2])
+{
+	int	*begin;
+
+	begin = stkbegin(cur) + 1;
+	if (cur == st[ST_A])
+	{
+		while (begin != stkrbegin(cur) && *begin < *(begin + 1))
+			++begin;
+	}
+	else
+	{
+		while (begin != stkrbegin(cur) && *begin > *(begin + 1))
+			++begin;
+	}
+	if (begin != stkrbegin(cur))
+		return (begin);
+	++begin;
+	return (begin);
 }
 
 void	reorderend(t_stack *a, t_stack *b, t_queue *instr_queue)
@@ -90,56 +136,110 @@ void	reorderend(t_stack *a, t_stack *b, t_queue *instr_queue)
 void	reorderpush(t_stack *cur, t_stack *st[2], const char *(*moveset[4])(),
 			t_queue *instr_queue)
 {
-	int *const	rbegin = stkrbegin(cur);
-	int			opposite_topval;
+	int	opposite_topval;
+	int	*ptr;
+	int	val;
 
 	if (cur == st[ST_A])
-		opposite_topval = stktop(st[ST_B]);
-	else
-		opposite_topval = stktop(st[ST_A]);
-	while (opposite_topval > stktop(cur))
-		add_op(moveset[ROT], st[ST_A], st[ST_B], instr_queue);
-	if (opposite_topval < stktop(cur))
 	{
-		while (*rbegin > opposite_topval)
-			add_op(moveset[RROT], st[ST_A], st[ST_B], instr_queue);
+		opposite_topval = stktop(st[ST_B]);
+		printf("%d, %d\n", opposite_topval, stktop(cur));
+		if (opposite_topval > stktop(cur))
+		{
+			ptr = stkbegin(cur);
+			while (ptr != stkrbegin(cur) && *ptr < opposite_topval && *(ptr + 1) > *ptr)
+				++ptr;
+			val = *ptr;
+			while (stktop(cur) != val)
+				add_op(moveset[ROT], st[ST_A], st[ST_B], instr_queue);
+		}
+		else
+		{
+			ptr = stkrbegin(cur);
+			if (*ptr < stktop(cur))
+			{
+				while (ptr != stkbegin(cur) && *ptr < opposite_topval && *ptr > *(ptr - 1))
+					--ptr;
+				val = *ptr;
+				while (stktop(cur) != val)
+					add_op(moveset[RROT], st[ST_A], st[ST_B], instr_queue);
+			}
+		}
+	}
+	else
+	{
+		opposite_topval = stktop(st[ST_A]);
+		printf("%d, %d\n", opposite_topval, stktop(cur));
+		if (opposite_topval < stktop(cur))
+		{
+			ptr = stkbegin(cur);
+			while (ptr != stkrbegin(cur) && *ptr > opposite_topval && *(ptr + 1) < *ptr)
+				++ptr;
+			if (ptr != stkrbegin(cur))
+			{
+				val = *ptr;
+				while (stktop(cur) != val)
+					add_op(moveset[ROT], st[ST_A], st[ST_B], instr_queue);
+			}
+		}
+		else
+		{
+			ptr = stkrbegin(cur);
+			if (*ptr > stktop(cur))
+			{
+				while (ptr != stkbegin(cur) && *ptr > opposite_topval && *ptr < *(ptr - 1))
+					--ptr;
+				val = *ptr;
+				while (stktop(cur) != val)
+					add_op(moveset[RROT], st[ST_A], st[ST_B], instr_queue);
+			}
+		}
 	}
 	add_op(moveset[PUSH], st[ST_A], st[ST_B], instr_queue);
 }
 
-size_t	streak_size(t_stack *st)
+size_t	streak_size(t_stack *cur, t_stack *st[2])
 {
 	size_t		size;
 	const int	*cbegin;
 	const int	*crbegin;
 
-	cbegin = stkcbegin(st);
-	crbegin = stkrcbegin(st);
-	size = 0;
-	if (*crbegin < *cbegin)
+	cbegin = stkcbegin(cur);
+	crbegin = stkrcbegin(cur);
+	size = 1;
+	if (cur == st[ST_A])
 	{
-		while (crbegin != cbegin && *(crbegin - 1) < *crbegin)
+		if (*crbegin < *cbegin)
 		{
-			--crbegin;
+			while (crbegin != cbegin && *(crbegin - 1) < *crbegin)
+			{
+				--crbegin;
+				++size;
+			}
+		}
+		while (cbegin != crbegin && *cbegin < *(cbegin + 1))
+		{
+			++cbegin;
 			++size;
 		}
 	}
-	while (cbegin != crbegin && *cbegin < *(cbegin + 1))
+	else
 	{
-		++cbegin;
-		++size;
+		if (*crbegin > *cbegin)
+		{
+			while (crbegin != cbegin && *(crbegin - 1) > *crbegin)
+			{
+				--crbegin;
+				++size;
+			}
+		}
+		while (cbegin != crbegin && *cbegin > *(cbegin + 1))
+		{
+			++cbegin;
+			++size;
+		}
 	}
 	return (size);
-}
-
-int	*next_streak(t_stack *st)
-{
-	int	*begin;
-
-	begin = stkbegin(st);
-	while (begin != stkrbegin(st) && *begin < *(begin + 1))
-		++begin;
-	return (begin);	
 }
 
 int	get_instruction_queue(t_queue *instr_queue, t_stack *a, t_stack *b,
@@ -149,52 +249,46 @@ int	get_instruction_queue(t_queue *instr_queue, t_stack *a, t_stack *b,
 	int		*ptr;
 	int		val;
 
+	print_stack(a);
+	print_stack(b);
+
 	reorderstart(a, (t_stack *[2]){a, b}, (const char *(*[4])()){sa, ra, rra,
 		pb}, instr_queue);
 	while (stksize(a) > 3 * size / 4)
 	{
 		if (stktop(a) > *(stkbegin(a) + 1))
 			add_op(sa, a, b, instr_queue);
-		strk_size = streak_size(a);
+		strk_size = streak_size(a, (t_stack *[2]){a, b});
 		while (strk_size--)
 			add_op(pb, a, b, instr_queue);
 	}
-	while (streak_size(a) != stksize(a))
+	kek();
+	while (streak_size(a, (t_stack *[2]){a, b}) != stksize(a) || streak_size(b, (t_stack *[2]){a, b}) != stksize(b))
 	{
-		ptr = next_streak(a);
+		printf("%zu :: %zu\n", streak_size(a, (t_stack *[2]){a, b}), streak_size(b, (t_stack *[2]){a, b}));
+		ptr = next_streak(a, (t_stack *[2]){a, b});
 		while (ptr != stkend(a))
 		{
 			val = *ptr;
 			while (stktop(a) != val)
 				reorderpush(b, (t_stack *[2]){a, b},
 					(const char *(*[4])()){sb, rb, rrb, pb}, instr_queue);
-			ptr = next_streak(a);
+			ptr = next_streak(a, (t_stack *[2]){a, b});
 		}
 		reorderstart(b, (t_stack *[2]){a, b}, (const char *(*[4])()){sb, rb,
 			rrb, pa}, instr_queue);
-		while (stksize(b) > 3 * size / 4)
-		{
-			strk_size = streak_size(b);
-			while (strk_size--)
-				add_op(pa, a, b, instr_queue);
-		}
-		ptr = next_streak(b);
+		ptr = next_streak(b, (t_stack *[2]){a, b});
 		while (ptr != stkend(b))
 		{
 			val = *ptr;
 			while (stktop(b) != val)
 				reorderpush(a, (t_stack *[2]){a, b}, (const char *(*[4])()){sa,
 					ra, rra, pa}, instr_queue);
-			ptr = next_streak(b);
+			kek();
+			ptr = next_streak(b, (t_stack *[2]){a, b});
 		}
 		reorderstart(a, (t_stack *[2]){a, b}, (const char *(*[4])()){sa, ra,
 			rra, pb}, instr_queue);
-		while (stksize(a) > 3 * size / 4)
-		{
-			strk_size = streak_size(a);
-			while (strk_size--)
-				add_op(pb, a, b, instr_queue);
-		}
 	}
 	while (!stkempty(b))
 		reorderpush(a, (t_stack *[2]){a, b}, (const char *(*[4])()){sa,
